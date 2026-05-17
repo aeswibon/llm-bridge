14584966092489b80f7c951f87d57c8eee87b454
Abhiuday <gupta.abhiuday.109@gmail.com>
2026-05-18 00:41:45 +0530

    feat(windsurf): add daemon manager for language server discovery

--- packages/windsurf/src/index.ts
+++ packages/windsurf/src/index.ts
@@ -1,3 +1,4 @@
 export { WindsurfBridgePlugin } from './plugin.js';
 export { WINDSURF_MODELS } from './models.js';
+export { createWindsurfDaemon } from './daemon.js';
 export type { WindsurfModel, WindsurfConfig } from './types.js';
